bc8c13e84da8525d9851e53a7050fb0eacb13369
Oscar Aponte Aguilar <ogaponte@gmail.com>
2024-02-23 20:04:24 +0000

    fix: Tipo de documento por default operacion interna para PV

--- models/pos_order.py
+++ models/pos_order.py
@@ -9,6 +9,8 @@ class PosOrder(models.Model):
     _inherit = "pos.order"
     
     def _create_invoice(self, move_vals):
+        doc_type_fel = self.env['dgi.tipo.documento'].sudo().search([('codigo','=','01')],limit=1)
+        move_vals['tipo_documento_fel'] = doc_type_fel.id;
         new_move = super(PosOrder, self)._create_invoice(move_vals)
         new_move.crear_folio_fel()
         return new_move
